cacbf5acc51b39668d30cdd78b60066fa08ad30a
TeresaRP94 <67110078+TeresaRP94@users.noreply.github.com>
2023-06-13 12:28:50 +0200

    Add files via upload

--- src/routes/tabs/tab-map/index.js
+++ src/routes/tabs/tab-map/index.js
@@ -31,7 +31,7 @@ export default class ViewTabMap extends HTMLElement {
     this._parent = document.querySelector('view-tabs')
     this._generateLayer()
 
-    this.mMap = new M.map({
+    this.mMap = M.map({
       container: 'tmap',
       center: [236403, 4141785],
       zoom: 3,
